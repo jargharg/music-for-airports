132504e57a1025cf1cb669cf36eeafd36a7e12e6
Jarod <jarod.hargreaves@gmail.com>
2021-06-29 21:23:06 +0100

    fix: add start buton to initialise audio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './style.css';
 import App from './components/App';
 
-App.init();
+(function () {
+	let startButton = document.querySelector('#startButton');
+	startButton.addEventListener('click', () => {
+		startButton.remove();
+		App.init();
+	});
+})();
